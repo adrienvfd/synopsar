ba8aa8e645f6e329a12210051cceb6900c161a4a
Adrien Vonfeld <vonfeld@gmail.com>
2024-04-21 22:49:20 +0300

    Groq Api pre-setup

---
+++ llm/llm.py
@@ -0,0 +1,55 @@
+from json import loads
+from os import getenv
+
+from addict import Dict
+from dotenv import load_dotenv
+from groq import Groq
+from regex import compile
+
+load_dotenv()
+
+
+def extract_json(string):
+    pattern = compile(r"\{(?:[^{}]|(?R))*\}")
+    matches = pattern.findall(string)
+    if matches:
+        return matches[0]
+
+
+class LLM:
+    def __init__(self):
+        self.api = Groq(api_key=getenv("GROQ_API_KEY"))
+
+    def call(self, system_prompt):
+        content, retry = None, 0
+        while not content and retry < 3:
+            try:
+                chat_completion = self.api.chat.completions.create(
+                    model="mixtral-8x7b-32768",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": system_prompt,
+                        }
+                    ],
+                )
+                content = chat_completion.choices[0].message.content
+            except Exception as e:
+                Log.error("LLM Error: " + str(e))
+        return content
+
+    def call_for_json(self, system_prompt):
+        formatted, retry = None, 0
+        while not formatted and retry < 3:
+            content = self.call(system_prompt)
+            try:
+                formatted = Dict(loads(extract_json(content)))
+            except Exception as e:
+                Log.error(
+                    "JSON Error: "
+                    + content
+                    + "\nExtracted: "
+                    + str(extract_json(content))
+                )
+            retry += 1
+        return formatted
---
+++ llm/prompts.py
@@ -0,0 +1,4 @@
+class Prompts:
+    summarize = """Summarize this youtube transcript into a structured essay. Keep it concise.
+    It must have a title, an introduction, a conclusion, and a list of key points.
+    """
